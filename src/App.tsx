8a27088f7db7ecd1913bcfa0e27718dbf5e7937b
塩月康大 <0711dr5@gmail.com>
2022-03-04 11:23:04 +0900

    全体のスタイルを設定（Chakra UI）

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 // import React from 'react';
 import { Button, ChakraProvider } from '@chakra-ui/react'
-import './App.css'
-
+import theme from './theme/theme'
 function App() {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Button colorScheme="teal">ボタン</Button>
     </ChakraProvider>
   )
